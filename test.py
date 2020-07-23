e8f0d48ae4b9c02df0302d4ee236883046fa23fa
Jesse Hendler <jessehendle2@gmail.com>
2020-07-23 16:23:45 -0400

    Updated files

--- test.py
+++ test.py
@@ -1,1 +1,73 @@
-print("Hello")
+a = 5
+b = 6
+c = a+b
+d = a**b
+print(a,b,c,d)
+print("%s %s %s %s" % (a,b,c,d))
+print(a,"is a number")
+print("%s is a number" % a)
+
+num = 150
+digit = 5
+num_str = str(num)
+digit_str = str(digit)
+digit_is_in_number = digit_str in num_str
+print(digit_is_in_number)
+
+l = [1,2,3,4]
+print(4 in l)
+
+# comments
+"""
+like this
+comments on multiple lines
+"""
+
+def add_a_and_b(a,b):
+    return a+b
+
+func = add_a_and_b(2,5)
+print(func)
+
+for i in range(10):
+    print(i)
+
+print()
+
+for j in range(5,10):
+    print(j)
+
+x = True
+y = False
+if x is True:
+    print("X is true")
+if y is True:
+    print("Y is true")
+if x == False and y == False:
+    print("Both false")
+else:
+    print("Both not false")
+
+def print_odd(limit):
+    for i in range(1,limit,2):
+        print(i)
+
+print_odd(10)
+
+# list = []
+# tuple = ()
+# dictionary = {}
+
+l1 = [1,5,10]
+t1 = (1,5,10)
+d1 = {
+    "US" : "United States",
+    "MX" : "Mexico",
+    "CA" : "Canada"
+}
+
+print(d1["US"])
+print(d1.keys())
+print(d1.items())
+print(d1.values())
+
---
+++ test2.py
@@ -0,0 +1,7 @@
+def penis(limit=10):
+    for i in range(0,limit):
+        print(i)
+    for i in range(0,limit):
+        print(i **2)
+
+penis(20)
